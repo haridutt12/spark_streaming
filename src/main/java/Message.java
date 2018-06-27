4057062c58bd3154e0b4ff8ec746ab48667602f7
Aayush <aayushtiwari@gmail.com>
2018-06-27 19:22:48 +0530

    files

---
+++ src/main/java/Message.java
@@ -0,0 +1,13 @@
+public class Message {
+    private String message;
+
+    public Message(){}
+
+    public String getMessage(){
+        return this.message;
+    }
+
+    public void setMessage(String myMessage){
+        this.message = myMessage;
+    }
+}
