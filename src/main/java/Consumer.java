b2fccb8132e9476b23056d899e34f5d4e5205d33
Hari <ashariduttparashar@gmail.com>
2018-06-28 20:19:14 +0530

    message stream to dataframe as it is

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -20,6 +20,7 @@ import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Consumer {
@@ -28,8 +29,6 @@ public class Consumer {
 
         SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("pulsar-spark");
 
-//        Duration duration = new Duration(6000);
-//        JavaStreamingContext jssc = new JavaStreamingContext(conf, duration);
      JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(5));
 
         ClientConfiguration clientConf = new ClientConfiguration();
@@ -40,37 +39,34 @@ public class Consumer {
 
         JavaReceiverInputDStream<byte[]> msgs = jssc.receiverStream(new SparkStreamingPulsarReceiver(clientConf, consConf, url, topic, subs));
 
-        JavaDStream<Integer> isContainingPulsar = msgs.flatMap((FlatMapFunction<byte[], Integer>) (byte[] msg) -> {
-            return Arrays.asList(((new String(msg)).indexOf("Pulsar") != -1) ? 1 : 0).iterator();
+        JavaDStream<String> message = msgs.flatMap((FlatMapFunction<byte[], String>) (byte[] msg) -> {
+            return Arrays.asList(new String(msg)).iterator();
             //return 10;
         });
 
-        JavaDStream<Integer> numOfPulsar = isContainingPulsar.reduce(
-                (Function2<Integer, Integer, Integer>) (i1, i2) -> i1 + i2);
-        numOfPulsar.print();
+//          JavaDStream<String> message = msgs.flatMap((FlatMapFunction<byte[], String>));
+//        JavaDStream<Integer> numOfPulsar = isContainingPulsar.reduce(
+//                (Function2<Integer, Integer, Integer>) (i1, i2) -> i1 + i2);
+//
+//        numOfPulsar.print();
 
 
-        numOfPulsar.foreachRDD(new VoidFunction<JavaRDD<Integer>>() {
-            @Override
-            public void call(JavaRDD<Integer> rdd) {
-                JavaRDD<Row> rowRDD = rdd.map(new Function<Integer, Row>() {
-                    @Override
-                    public Row call(Integer msg) {
-                        Row row = (Row) RowFactory.create(msg);
-                        return row;
-                    }
-                });
+            message.foreachRDD((VoidFunction<JavaRDD<String>>) rdd -> {
+            JavaRDD<Row> rowRDD = rdd.map((Function<String, Row>) msg -> {
+                Row row =  RowFactory.create(msg);
+                return row;
+            });
 
-                //Create Schema
-                StructType schema = DataTypes.createStructType(new StructField[] {DataTypes.createStructField("Message", DataTypes.StringType, true)});
+            //Create Schema
+            StructType schema = DataTypes.createStructType(new StructField[] {DataTypes.createStructField("Message", DataTypes.StringType
+                    , true)});
 
-                //Get Spark 2.0 session
-                SparkSession spark = JavaSparkSessionSingleton.getInstance(rdd.context().getConf());
+            //Get Spark 2.0 session
+            SparkSession spark = JavaSparkSessionSingleton.getInstance(rdd.context().getConf());
 
-                Dataset<Row> msgDataFrame = spark.createDataFrame(rowRDD, Message.class);
-                msgDataFrame.printSchema();
-                msgDataFrame.show();
-            }
+            Dataset<Row> msgDataFrame = spark.createDataFrame(rowRDD, schema);
+            msgDataFrame.printSchema();
+            msgDataFrame.show();
         });
 
 
